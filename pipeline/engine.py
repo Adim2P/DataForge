a74e2b9d29be806e24db5d4fa3b7b8efe1245909
Adim2P <miguellicauco@gmail.com>
2026-07-03 18:21:34 +0800

    Add: Validation Pipeline

---
+++ pipeline/engine.py
@@ -0,0 +1,56 @@
+from openpyxl import Workbook
+from logger import get_logger
+from pipeline.result import CellValidation
+from config import VALIDATE_FORMULAS
+
+logger = get_logger(__name__)
+
+class ValidationEngine:
+
+    def __init__(self, validators: list):
+        self.validators = validators
+
+    def run(self, workbook: Workbook):
+        results = []
+
+        logger.info("Starting Validation pipeline.")
+
+        for worksheet in workbook.worksheets:
+            logger.info(f"Scanning worksheet: {worksheet.title}")
+
+            for row in worksheet.iter_rows():
+                for cell in row:
+                    if cell.data_type == "f" and not VALIDATE_FORMULAS:
+                        continue
+
+                    if cell.value is None:
+                        continue
+
+                    if not isinstance(cell.value, str):
+                        continue
+
+                    if not cell.value.strip():
+                        continue
+
+                    for validator in self.validators:
+                        result = validator.validate(cell.value)
+
+                        if result:
+                            logger.info(
+                                f"[{validator.name}] "
+                                f"{worksheet.title}!{cell.coordinate}"
+                                f"-> {cell.value}"
+                            )
+
+                            results.append(
+                                CellValidation(
+                                    cell=cell,
+                                    result=result
+                                )
+                            )
+        logger.info(
+            f"Validation completed. {len(results)} issue(s) found."
+        )
+
+        return results
+        
